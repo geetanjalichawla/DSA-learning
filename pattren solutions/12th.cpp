44ea9de7bf5cb865f15ed0b7dc503e1eb7bd6dcb
geetc <geetc1919@gmail.com>
2022-04-20 19:06:18 +0530

    commit

--- pattren solutions/12th.cpp
+++ pattren solutions/12th.cpp
@@ -2,7 +2,7 @@
 // 1. space in every case will be row - 1 times 
 // 2 .and if the row number is 1 or n mwe have to treat diffrently 
 // 3. in 2 to n-1 part we have do someting
-// hollow pyramid invert
+
 #include<iostream>
 using namespace std;
 int main()
