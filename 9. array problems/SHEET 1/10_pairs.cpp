5865408e32b1dfaa7bd16e7c9b60a2330a9bc6b3
geetc <geetc1919@gmail.com>
2022-04-28 23:21:00 +0530

    commit

--- 9. array problems/SHEET 1/10_pairs.cpp
+++ 9. array problems/SHEET 1/10_pairs.cpp
@@ -2,24 +2,41 @@
 using namespace std;
 #include<algorithm>
 #include<vector>
-vector<vector<int>> pairSum(vector<int> &arr, int s){
+vector<vector<int>> pairSumBootforce(vector<int> &arr, int s){
    // Write your code here
-	sort(arr.begin(),arr.end());
+	sort(arr.begin(),arr.end());// no need in case of normal this is for 
 	vector< vector<int> > ans;
 	
 	for(int i = 0 ; i < arr.size() ; i++){
 		for(int j = i+1 ; j<arr.size(); j++)
 		{
 			if(arr[i] + arr[j] == s)
-				{vector<int> pair(2);
+				{
+                vector<int> pair(2);
                 pair[0] = arr[i];
                 pair[1] = arr[j];
-                ans.push_back(pair);}
+                ans.push_back(pair);
+                }
 
 		}
 	}
 	return ans;
 }
+void pairSum(vector<int> &arr, int s){
+   // Write your code here
+	sort(arr.begin(),arr.end());// need in case  
+	
+	int i = 0;
+    int j = arr.size()-1;
+    while (i<j)
+    {
+        if(arr[i]+arr[j] == s) {cout<<arr[i]<<" "<<arr[j]<<endl; i++;}
+        else if(arr[i]+arr[j] > s) j--;
+        else i ++;
+    }
+}
+
+
 void pntVector(vector<vector <int>> ans)
 {
     for (int i = 0; i < ans.size(); i++)
@@ -36,8 +53,7 @@ void pntVector(vector<vector <int>> ans)
 int main()
 {
     vector<int> arr = {1,3,4,4,41,40,39,38,35,33,-44,5,2,3,34,34,4,532,432,341,4141,341,5,235,4,5,5,23,23,34,34,34,32,34,4,52,423,42,4,234,23,4,23,4,5,234,23,4,23,23,42,34,2334,2,43,234,23,432,423,4,23,423,4,32432,4,3,4,34,3,34,34,43,43,43,34,2,34,34,3,34,2};
-    vector<vector <int>> ans = pairSum(arr,44);
-    pntVector(ans);
+   pairSum(arr, 40);
 
 return 0;
 }
--- 9. array problems/SHEET 1/15_kadanealgo.cpp
+++ 9. array problems/SHEET 1/15_kadanealgo.cpp
@@ -1,25 +1,45 @@
 #include<iostream>
 #include<limits.h>
 using namespace std;
+void printarr(int arr[], int s , int n);
 int maxsubarray(int arr[], int n){
     int sum = 0;
     int ans = INT_MIN;
+    int start = 0;
+    int end = 0;
+    int store = 0;
     for (int i = 0; i < n; i++)
     {
         sum = sum + arr[i];
+        if(sum>ans)
+        {
+            start = store;
+            end = i;
+        }
         ans = max(ans,sum);
         if (sum < 0)
         {    sum = 0;
+            store = i+1;
         }
         
     }
-    return ans;
+    printarr(arr,start,end);
+    cout<<"\nsize of max sub array is  ";
+
+  return ans;
     
 
+}
+void printarr(int arr[], int s , int n)
+{
+    for (int i = s; i <= n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 }
 int main()
 {
 int arr[] = {-2,-3,4,-1,-2,1,5,-3};
-cout<<"size of max sub array is"<<maxsubarray(arr,8);
+cout<<maxsubarray(arr,8);
 return 0;
 }
