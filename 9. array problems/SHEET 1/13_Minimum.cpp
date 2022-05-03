a21c952531d83c4807d1eb1e3a37080d5c4127ff
geetc <geetc1919@gmail.com>
2022-05-03 22:25:26 +0530

    are yaar ho gya

--- 9. array problems/SHEET 1/13_Minimum.cpp
+++ 9. array problems/SHEET 1/13_Minimum.cpp
@@ -2,30 +2,29 @@
 using namespace std;
 int  minswap(int k , int arr[], int n)
 {
-    int i = 0;
-    int j = 0;
-    int count = 0;
-    while (i<n && j< n)
+    int count = 0;   
+    for (int i = 0; i < n; i++)
     {
-        if (arr[j] < k || i==j){
-           if (i != j)
-           {
-               swap(arr[i], arr[j]);
-               count++;
-           }
-           
-            i++;
-            j++;
+        if(arr[i] <= k){
+            count++;
         }
-        if (arr[i] <= k){
-            i++;
-        }
-        if(arr[j] > k){
-            j++;
-        }
-   }
-    return count;
+    }
+    int start = 0, end = count-1,bad = 0,move = n;
+    while (end<n)
+    {
+       
+    for (int i = start; i <= end; i++)
+    {
+        //  cout<<start<<" i = "<<i<<" "<<end<<" "<<bad<<endl;
+    }
+    move = min(bad,move);
+    // cout<<move<<" "<<bad<<"at the "<<start<<end<<endl;
+    bad = 0 ;
+    start++;
+    end++;
     
+    }
+   return move;
 
 }
 void pntVector(int ans[], int n)
@@ -38,8 +37,7 @@ int main()
 {
 int a[]=   {9, 7, 2, 5, 4, 7, 8};
  pntVector(a,7);
- cout<<"\n"<<minswap(8,a,7);
+ cout<<"\n answer for min swap shoubld be "<<minswap(7,a,7);
  cout<<endl;
- pntVector(a,7);
 return 0;
 }
